5dc624f3d1e0f94b857e88c7301e06f452982f05
Andrey Prokopenko <prokopenkoav@ornl.gov>
2023-04-02 11:08:18 -0400

    [wip] Plasma example

---
+++ examples/plasma/example_plasma.cpp
@@ -0,0 +1,26 @@
+/****************************************************************************
+ * Copyright (c) 2023 by the ArborX authors                                 *
+ * All rights reserved.                                                     *
+ *                                                                          *
+ * This file is part of the ArborX library. ArborX is                       *
+ * distributed under a BSD 3-clause license. For the licensing terms see    *
+ * the LICENSE file in the top-level directory.                             *
+ *                                                                          *
+ * SPDX-License-Identifier: BSD-3-Clause                                    *
+ ****************************************************************************/
+
+#include <ArborX.hpp>
+
+#include <Kokkos_Core.hpp>
+
+int main(int argc, char *argv[])
+{
+  Kokkos::ScopeGuard guard(argc, argv);
+
+  using ExecutionSpace = Kokkos::DefaultExecutionSpace;
+  using MemorySpace = typename ExecutionSpace::memory_space;
+
+  ExecutionSpace execution_space;
+
+  return EXIT_SUCCESS;
+}
