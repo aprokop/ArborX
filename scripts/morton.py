a481421dd6afa7e95f6c50b68d517c22ca08b22d
Andrey Prokopenko <prokopenkoav@ornl.gov>
2023-05-24 20:22:12 -0400

    Convert to Python3 and remove some print outs

--- scripts/morton.py
+++ scripts/morton.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # Taken from https://stackoverflow.com/questions/1024754/how-to-compute-a-3d-morton-number-interleave-the-bits-of-3-ints/18528775#18528775
 
 import random
@@ -39,12 +39,8 @@ def binStr(x):
 
 def computeBitMaskPatternAndCode(numberOfBits, numberOfEmptyBits):
     bitDistances = [i * numberOfEmptyBits for i in range(numberOfBits)]
-    print("Bit Distances: " + str(bitDistances))
     bitDistancesB = [bin(dist)[2:] for dist in bitDistances]
-    print("Bit Distances (binary): " + str(bitDistancesB))
-    moveBits = (
-        []
-    )
+    moveBits = ([])
 
     maxLength = len(max(bitDistancesB, key=len))
     for i in range(maxLength):
@@ -54,52 +50,25 @@ def computeBitMaskPatternAndCode(numberOfBits, numberOfEmptyBits):
                 if bits[len(bits) - i - 1] == "1":
                     moveBits[i].append(idx)
 
-    for i in range(len(moveBits)):
-        print(
-            "Shifting bits by "
-            + str(2 ** i)
-            + "\t for bits idx: "
-            + str(moveBits[i])
-        )
-
-    bitPositions = range(numberOfBits)
-    print("BitPositions: " + str(bitPositions))
+    bitPositions = list(range(numberOfBits))
     maskOld = (1 << numberOfBits) - 1
 
     codeString = "x &= " + hex(maskOld) + "\n"
-    for idx in xrange(len(moveBits) - 1, -1, -1):
+    for idx in range(len(moveBits) - 1, -1, -1):
         if len(moveBits[idx]):
 
             shifted = 0
             for bitIdxToMove in moveBits[idx]:
                 shifted |= 1 << bitPositions[bitIdxToMove]
                 bitPositions[bitIdxToMove] += 2 ** idx
-                # keep track where the actual bit stands! might get moved several times
+                # keep track where the actual bit stands! might get moved
+                # several times
 
             # Get the non shifted part!
             nonshifted = ~shifted & maskOld
 
-            print(
-                "Shifted bef.:\t" + binStr(shifted) + " hex: " + hex(shifted)
-            )
             shifted = shifted << 2 ** idx
-            print("Shifted:\t" + binStr(shifted) + " hex: " + hex(shifted))
-
-            print(
-                "NonShifted:\t"
-                + binStr(nonshifted)
-                + " hex: "
-                + hex(nonshifted)
-            )
             maskNew = shifted | nonshifted
-            print(
-                "Bitmask is now:\t"
-                + binStr(maskNew)
-                + " hex: "
-                + hex(maskNew)
-                + "\n"
-            )
-            # print("Code: " + "x = x | x << " +str(2**idx)+ " & " +hex(maskNew))
 
             codeString += (
                 "x = (x | x << " + str(2 ** idx) + ") & " + hex(maskNew) + "\n"
@@ -112,35 +81,3 @@ numberOfBits = 21
 numberOfEmptyBits = 2
 codeString = computeBitMaskPatternAndCode(numberOfBits, numberOfEmptyBits)
 print(codeString)
-
-
-def partitionBy2(x):
-    exec(codeString)
-    return x
-
-
-def checkPartition(x):
-    print("Check partition for: \t" + binStr(x))
-    part = partitionBy2(x)
-    print("Partition is : \t\t" + binStr(part))
-    # make the pattern manualy
-    partC = long(0)
-    for bitIdx in range(numberOfBits):
-        partC = partC | (x & (1 << bitIdx)) << numberOfEmptyBits * bitIdx
-    print("Partition check is :\t" + binStr(partC))
-    if partC == part:
-        return True
-    else:
-        return False
-
-
-checkError = False
-for i in range(20):
-    x = random.getrandbits(numberOfBits)
-    if checkPartition(x) is False:
-        checkError = True
-        break
-if not checkError:
-    print("CHECK PARTITION SUCCESSFUL!!!!!!!!!!!!!!!!...")
-else:
-    print("checkPartition has ERROR!!!!")
