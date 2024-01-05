7b110db7a92ae82a63ab77328c486c432c0c5b39
Andrey Prokopenko <prokopenkoav@ornl.gov>
2024-01-05 15:08:39 -0500

    Switch to using Kokkos::{min,max} post 4.3

--- src/kokkos_ext/ArborX_DetailsKokkosExtMinMaxOperations.hpp
+++ src/kokkos_ext/ArborX_DetailsKokkosExtMinMaxOperations.hpp
@@ -13,65 +13,14 @@
 #define ARBORX_DETAILS_KOKKOS_EXT_MIN_MAX_OPERATIONS_HPP
 
 #include <Kokkos_Macros.hpp>
-
-#include <initializer_list>
+#include <Kokkos_MinMax.hpp>
 
 namespace ArborX::Details::KokkosExt
 {
 
-//! Compute the maximum of two values.
-template <typename T>
-KOKKOS_INLINE_FUNCTION constexpr T const &max(T const &a, T const &b)
-{
-  return (a > b) ? a : b;
-}
-
-//! Compute the minimum of two values.
-template <typename T>
-KOKKOS_INLINE_FUNCTION constexpr T const &min(T const &a, T const &b)
-{
-  return (a < b) ? a : b;
-}
-
-template <class T>
-KOKKOS_INLINE_FUNCTION constexpr T max(std::initializer_list<T> ilist)
-{
-  auto const *first = ilist.begin();
-  auto const *const last = ilist.end();
-  auto result = *first;
-  if (first == last)
-  {
-    return result;
-  }
-  while (++first != last)
-  {
-    if (result < *first)
-    {
-      result = *first;
-    }
-  }
-  return result;
-}
-
-template <class T>
-KOKKOS_INLINE_FUNCTION constexpr T min(std::initializer_list<T> ilist)
-{
-  auto const *first = ilist.begin();
-  auto const *const last = ilist.end();
-  auto result = *first;
-  if (first == last)
-  {
-    return result;
-  }
-  while (++first != last)
-  {
-    if (*first < result)
-    {
-      result = *first;
-    }
-  }
-  return result;
-}
+using Kokkos::max;
+using Kokkos::min;
+using Kokkos::minmax;
 
 } // namespace ArborX::Details::KokkosExt
 
