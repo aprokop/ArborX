b339796b545b8629a30078b9c2acafb52178875e
Andrey Prokopenko <prokopenkoav@ornl.gov>
2024-10-02 12:04:19 -0400

    Clean up ArborX.hpp

--- src/ArborX.hpp
+++ src/ArborX.hpp
@@ -14,22 +14,28 @@
 
 #include <ArborX_Config.hpp>
 
+// Geometries
 #include <ArborX_Box.hpp>
+#include <ArborX_KDOP.hpp>
+#include <ArborX_Point.hpp>
+#include <ArborX_Ray.hpp>
+#include <ArborX_Segment.hpp>
+#include <ArborX_Sphere.hpp>
+#include <ArborX_Tetrahedron.hpp>
+#include <ArborX_Triangle.hpp>
+
+// Indexes
 #include <ArborX_BruteForce.hpp>
 #ifdef ARBORX_ENABLE_MPI
 #include <ArborX_DistributedTree.hpp>
 #endif
 #include <ArborX_CrsGraphWrapper.hpp>
 #include <ArborX_LinearBVH.hpp>
-#include <ArborX_Point.hpp>
-#include <ArborX_Sphere.hpp>
+#include <detail/ArborX_AttachIndices.hpp>
+#include <detail/ArborX_NeighborList.hpp>
 #include <detail/ArborX_PredicateHelpers.hpp>
-#include <detail/ArborX_Predicates.hpp>
+
+// Other files
 #include <misc/ArborX_Exception.hpp>
-// FIXME: we include ArborX_Utils.hpp only for backward compatibility for
-// users using deprecated functions in ArborX namespace (min, max,
-// adjacentDifference, ...). This header should be removed when we remove those
-// functions.
-#include <misc/ArborX_Utils.hpp>
 
 #endif
